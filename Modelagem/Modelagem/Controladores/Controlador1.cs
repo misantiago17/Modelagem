d6b34332b12b2adacb1e7ff19b40fbc8bfac68c0
michelle santiago <michelle.santiago10@gmail.com>
2019-05-27 15:42:31 -0300

    projeto iniciado com primeira classe controladora

---
+++ Modelagem/Modelagem/Controladores/Controlador1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelagem.Controladores
+{
+    class Controlador1
+    {
+        //JSON listaPedidoLoja;
+
+        void SalvaListaJSON()
+        {
+
+        }
+
+        void SalvaRegistroLoja()
+        {
+
+        }
+    }
+}
